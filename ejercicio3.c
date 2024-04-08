60116a299daedfe69eb4fdddd115945cc93c2da2
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-04-08 16:24:57 -0300

    Create ejercicio3.c

---
+++ ejercicio3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a;
+        scanf("%d", &a);
+    if(a % 2 == 0){
+        printf("el numero puesto es par");
+    }
+    else {
+        printf("el numero es impar");
+    }
+
+return 0;
+}
