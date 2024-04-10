ab7f851d4f8ad94edc10b19dc274191b16b711ef
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-04-10 11:50:12 -0300

    Create ejercicio12.c

---
+++ ejercicio12.c
@@ -0,0 +1,12 @@
+int main(){
+       int sb, v1, v2, v3;
+     scanf("%d", &sb);
+     scanf("%d", &v1);
+     scanf("%d", &v2);
+     scanf("%d", &v3);
+
+   int comision= (v1+v2+v3)*0.10;
+        printf("el sueldo del vendedor es de %d", sb+comision);
+   
+    return 0;
+}
